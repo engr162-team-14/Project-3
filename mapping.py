24af459332b78411418d0f06cdaf3653e5265a0c
Zach Ghera <zpghera00@gmail.com>
2019-03-17 19:57:05 -0400

    mapping updates

--- mapping.py
+++ mapping.py
@@ -7,89 +7,87 @@ import numpy as np
 from enum import Enum
 
 class State(Enum):
-    WALL = 0
+    UNEXPL = -1
+    UNKWN = 0
     EXPL = 1
-    UNEXPL = 2
+    ORIG = 5
+    HEAT = 2
+    MAG = 3
+    EXIT = 4
+
+class Unit(Enum):
+    CM = 0
+    IN = 1
 
 class Map:
-    def __init__(self,x_len = 50,y_len = 50):
-        self.grid = np.zeros((x_len,y_len))
+    def __init__(self, origin = [0,0], map_num = 0, team = 14, unit = Unit.CM):
+        self.origin = origin
+        self.grid = np.full((origin[1],origin[0]),State.UNKWN)
+        self.grid[-origin[1]][origin[0] - 1] = State.ORIG
+        self.map_num = map_num
+        self.team = team
+        self.unit = unit
+        self.hazard_info = [["Hazard Type","Parameter of Interest","Parameter Value","Hazard X Coordinate","Hazard Y Coordinate"]]
+        self.cur_pt = origin
+    @property
+    def origin(self):
+        return self.__origin
+    @origin.setter
+    def origin(self, origin):
+        self.__origin = origin
     @property
     def grid(self):
         return self.__grid
     @grid.setter
     def grid(self, grid):
         self.__grid = grid
+    @property
+    def map_num(self):
+        return self.__map_num
+    @map_num.setter
+    def map_num(self, map_num):
+        self.__map_num = map_num
+    @property
+    def team(self):
+        return self.__team
+    @team.setter
+    def team(self, team):
+        self.__team = team
+    @property
+    def unit(self):
+        return self.__unit
+    @unit.setter
+    def unit(self, unit):
+        self.__unit = unit
+    @property
+    def hazard_info(self):
+        return self.__hazard_info
+    @hazard_info.setter
+    def hazard_info(self, hazard_info):
+        self.__hazard_info = hazard_info
+    @property
+    def cur_pt(self):
+        return self.__cur_pt
+    @cur_pt.setter
+    def cur_pt(self, cur_pt):
+        self.__cur_pt = cur_pt
+    def addPoint(self,pt,point_type = State.EXPL):
+        pass
+    def updateLocation(self):
+        pass
     def findNearestUnexp(self,):
         pass
-    def pushMap(self,):
-        pass
-
-
-class GridSq:
-    def __init__(self,length = 5, up = State.UNEXPL, down = State.EXPL, left = State.WALL, right = State.WALL, \
-        next_up = None, next_down = None, next_left = None, next_right = None):
-        self.length = length
-        self.up = up
-        self.down = down
-        self.left = left
-        self.right = right
-        self.next_up = next_up
-        self.next_down = next_down
-        self.next_left = next_left
-        self.next_right = next_right
-    @property
-    def length(self):
-        return self.__length
-    @length.setter
-    def length(self,length):
-        self.__length = length
-    @property
-    def up(self):
-        return self.__up
-    @up.setter
-    def up(self,up):
-        self.__up = up
-    @property
-    def down(self):
-        return self.__down
-    @down.setter
-    def down(self,down):
-        self.__down = down
-    @property
-    def left(self):
-        return self.__left
-    @left.setter
-    def left(self,left):
-        self.__left = left
-    @property
-    def right(self):
-        return self.__right
-    @right.setter
-    def right(self,right):
-        self.__right = right
-    @property
-    def next_up(self):
-        return self.__next_up
-    @next_up.setter
-    def next_up(self,next_up):
-        self.__next_up = next_up
-    @property
-    def next_down(self):
-        return self.__next_down
-    @next_down.setter
-    def next_down(self,next_down):
-        self.__next_down = next_down
-    @property
-    def next_left(self):
-        return self.__next_left
-    @next_left.setter
-    def next_left(self,next_left):
-        self.__next_left = next_left
-    @property
-    def next_right(self):
-        return self.__next_right
-    @next_right.setter
-    def next_right(self,next_right):
-        self.__next_right = next_right
-
+    def _convertMap(self):
+        return None
+    def addHazard(self,haz_type,value,loc):
+        hazard_dict = {
+            State.HEAT: [" "," "],
+            State.MAG: [" "," "]
+        }
+        new_hazard = [hazard_dict[haz_type][0],hazard_dict[haz_type][1],value,loc[0],loc[1]]
+        self.hazard_info.append(new_hazard)
+    def pushInfo(self,):
+        info = {"map": self._convertMap(),
+                "hazard table": self.hazard_info 
+        }
+        return info
