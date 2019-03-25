9c5841b7ab1aef4b0f973a20e456d5fd0f2e6f2d
Zach Ghera <zpghera00@gmail.com>
2019-03-24 21:18:23 -0400

    mapping function

--- main.py
+++ main.py
@@ -67,6 +67,149 @@ def calibrate(BP):
 
     return imu_calib
 
+def mazeMap(BP,imu_calib,speed,set_dists,kp = .4,ki = .02,bfr_dist = 25,sensor = Sensor.LEFT,gyro_kp = .2,gyro_ki = 0.0):
+    '''set_dists = [front sensor stop dist, left sensor set pt, right senor set pt]'''
+    try:
+        errors = [-1,1,1]
+        errors_p = [0,0,0]
+        integs = [0,0,0]
+        dt = .2            #loop iteration ~= .15 + .05 sleep
+        cur_angle = sensors.gyroVal(BP)
+        act_dists = np.multiply(sensors.getUltras(BP), cos(radians(sensors.gyroVal(BP) - cur_angle)))
+        
+        while True:
+            # sweep to parallel with wall
+            dead_band_dist = .5
+            delt_ang1 = parallelToWall(BP, cur_angle, dtheta=30, sweep_spd = 2, sensor = sensor, dt = .05)
+            sensors.printGyroVal(BP)
+            print("delt_ang1:",delt_ang1)
+            cur_angle += delt_ang1
+            
+            print("sonar PID init")
+            while act_dists[0] > set_dists[0] and act_dists[1] < set_dists[1] + bfr_dist and act_dists[2] < set_dists[2] + bfr_dist:
+                errors = np.subtract(set_dists, act_dists)
+
+                if abs(errors[1]) <= dead_band_dist:
+                    print("inside dead band")
+
+                    dead_band_dist = 4
+
+                    # integral windup reset
+                    if sensor == Sensor.LEFT:
+                        integs[1] = 0
+                    elif sensor == Sensor.RIGHT:
+                        integs[2] = 0
+                    
+                    turnPi(BP,cur_angle - sensors.gyroVal(BP))
+
+                    gyro_error = -1
+                    gyro_error_p = 0
+                    gyro_integ = 0
+                    gyro_dt = .1
+
+                    dps = (speed * (360/(7* pi)))
+
+                    print("gyro PID init")
+                    while abs(act_dists[1] - set_dists[1]) < dead_band_dist and act_dists[0] > set_dists[0] and act_dists[2] < set_dists[2] + bfr_dist:
+                        gyro_error = cur_angle - sensors.gyroVal(BP)                         #error = system (gyro) dev from desired state (target_deg)
+                        gyro_integ = gyro_integ + (gyro_dt * (gyro_error + gyro_error_p)/2)  #integral feedback (trapez approx)
+                        gyro_output = gyro_kp * (gyro_error) + gyro_ki * (gyro_integ)        #PI feedback response
+                        gyro_error_p = gyro_error
+                        
+                        BP.set_motor_dps(BP.PORT_C, dps + gyro_output)   
+                        BP.set_motor_dps(BP.PORT_B, dps - gyro_output) 
+                        act_dists = np.multiply(sensors.getUltras(BP), cos(radians(sensors.gyroVal(BP) - cur_angle)))
+ 
+                        time.sleep(gyro_dt)
+                    dead_band_dist = .5
+                    print("sonar PID init")
+
+                else:    
+                    integs = np.add(integs, (np.multiply(dt, np.divide(np.add(errors, errors_p), 2))))
+
+                    outputs  = np.add(np.multiply(kp, errors), np.multiply(ki, integs))
+                    errors_p = errors
+
+                    if sensor == Sensor.LEFT:
+                        #apprch right wall --> (-) error --> subtr (-) error to right w (^ spd) & add (-) error to left w (v spd) 
+                        #apprch left wall --> negative error --> subtr (+) error to right w (v spd) & add (+) error to left w (^ spd)
+                        if(outputs[1] >= 0):
+                            setSpeed(BP, speed + outputs[1], speed) 
+                        else:
+                            setSpeed(BP, speed, speed - outputs[1])
+                    elif sensor == Sensor.RIGHT:
+                        #apprch right wall --> (+) error --> add (+) error to right w (^ spd) & subtr (+) error to left w (v spd) 
+                        #apprch left wall --> (-) error --> add (-) error to right w (v spd) & subtr (-) error to left w (^ spd)
+                        if(outputs[2] >= 0):
+                            setSpeed(BP, speed, speed + outputs[2]) 
+                        else:
+                            setSpeed(BP, speed - outputs[2], speed)  
+
+                act_dists = np.multiply(sensors.getUltras(BP), cos(radians(sensors.gyroVal(BP) - cur_angle)))
+
+                time.sleep(.05)
+            
+            setSpeed(BP,0,0)
+            print("junction case reached")
+            print("front: %d | left: %d | right: %d" % (act_dists[0],act_dists[1],act_dists[2]))
+            turnPi(BP,cur_angle - sensors.gyroVal(BP))
+
+            #check for junction cases
+            cur_front = act_dists[0]
+            cur_left = act_dists[1]
+            cur_right = act_dists[2] 
+
+            #dead end
+            if cur_front <= set_dists[0] and cur_left <= set_dists[1] + bfr_dist and cur_right <= set_dists[2] + bfr_dist:
+                turn_ang = 180
+                print("dead end")
+            #left option only
+            elif cur_front <= set_dists[0] and cur_left >= set_dists[1] + bfr_dist and cur_right <= set_dists[2] + bfr_dist:
+                turn_ang = -90
+                print("left option only")
+            #right option only
+            elif cur_front <= set_dists[0] and cur_left <= set_dists[1] + bfr_dist and cur_right >= set_dists[2] + bfr_dist:
+                turn_ang = 90
+                print("right option only")
+            #right and left options
+            elif cur_front <= set_dists[0] and cur_left >= set_dists[1] + bfr_dist and cur_right >= set_dists[2] + bfr_dist:
+                turn_ang = 90
+                print("right and left options")
+            #left and forward
+            elif cur_front >= set_dists[0] and cur_left >= set_dists[1] + bfr_dist and cur_right <= set_dists[2] + bfr_dist:
+                turn_ang = -90
+                print("left and forward options")
+            #right and forward
+            elif cur_front >= set_dists[0] and cur_left <= set_dists[1] + bfr_dist and cur_right >= set_dists[2] + bfr_dist:
+                turn_ang = 90
+                print("right and forward options")
+            #4 way intersection
+            elif cur_front >= set_dists[0] and cur_left >= set_dists[1] + bfr_dist and cur_right >= set_dists[2] + bfr_dist:
+                turn_ang = 90
+                print("4 way intersection")
+            else:
+                turn_ang = 0
+                print("Well, sheit...problems...")
+
+            speedControl(BP,imu_calib,speed,10)
+            cur_angle += turn_ang
+            turnPi(BP,turn_ang)
+
+            #drive forward until walls on each side and path ahead (normal)
+            act_dists = np.multiply(sensors.getUltras(BP), cos(radians(sensors.gyroVal(BP) - cur_angle)))
+            while act_dists[0] <= set_dists[0] or act_dists[1] >= set_dists[1] + bfr_dist or act_dists[2] >= set_dists[2] + bfr_dist:
+                setSpeed(BP,speed,speed)
+                act_dists = np.multiply(sensors.getUltras(BP), cos(radians(sensors.gyroVal(BP) - cur_angle)))
+                time.sleep(.1)
+            speedControl(BP,imu_calib,speed,5)
+
+            setSpeed(BP,0,0)
+           
+    except Exception as error: 
+        print("mazeMap:",error)
+    except KeyboardInterrupt:
+        stop(BP)
+
 def mazeNav(BP,imu_calib,speed,set_dists,kp = .4,ki = .02,bfr_dist = 25,sensor = Sensor.LEFT,gyro_kp = .2,gyro_ki = 0.0):
     '''set_dists = [front sensor stop dist, left sensor set pt, right senor set pt]'''
     try:
@@ -155,13 +298,12 @@ def mazeNav(BP,imu_calib,speed,set_dists,kp = .4,ki = .02,bfr_dist = 25,sensor =
             turnPi(BP,cur_angle - sensors.gyroVal(BP))
 
             #check for junction cases
-            # cur_front = sensors.getUltras(BP)[0]
-            # cur_left = sensors.getUltras(BP)[1]
-            # cur_right = sensors.getUltras(BP)[2]
+            ###cur_front = sensors.getUltras(BP)[0]
+            ###cur_left = sensors.getUltras(BP)[1]
+            ###cur_right = sensors.getUltras(BP)[2]
             cur_front = act_dists[0]
             cur_left = act_dists[1]
-            cur_right = act_dists[2]
-           
+            cur_right = act_dists[2] 
 
             #dead end
             if cur_front <= set_dists[0] and cur_left <= set_dists[1] + bfr_dist and cur_right <= set_dists[2] + bfr_dist:
--- mapping.py
+++ mapping.py
@@ -6,11 +6,11 @@ import time
 import numpy as np
 from enum import Enum
 
-# class Dir(Enum):
-#     UP = 0
-#     DOWN = 1
-#     LEFT = 2
-#     RIGHT = 3
+class Dir(Enum):
+    UP = 0
+    DOWN = 1
+    LEFT = 2
+    RIGHT = 3
 
 class State(Enum):
     UNEXPL = -1
@@ -27,7 +27,7 @@ class Unit(Enum):
     IN = 1
 
 class Map:
-    def __init__(self, origin = [0,0], map_num = 0, team = 14, unit = Unit.CM):
+    def __init__(self, origin = [0,0], map_num = 0, team = 14, unit = Unit.CM, direc = Dir.UP):
         self.origin = origin
         self.grid = np.full((origin[1] + 1,origin[0] + 1),State.UNKWN)
         self.grid[-origin[1] - 1][origin[0]] = State.ORIG
@@ -35,7 +35,8 @@ class Map:
         self.team = team
         self.unit = unit
         self.hazard_info = [["Hazard Type","Parameter of Interest","Parameter Value","Hazard X Coordinate","Hazard Y Coordinate"]]
-        self.cur_pt = origin
+        self.cur_loc = origin
+        self.cur_direc = direc
     @property
     def origin(self):
         return self.__origin
@@ -73,11 +74,17 @@ class Map:
     def hazard_info(self, hazard_info):
         self.__hazard_info = hazard_info
     @property
-    def cur_pt(self):
-        return self.__cur_pt
-    @cur_pt.setter
-    def cur_pt(self, cur_pt):
-        self.__cur_pt = cur_pt
+    def cur_loc(self):
+        return self.__cur_loc
+    @cur_loc.setter
+    def cur_loc(self, cur_loc):
+        self.__cur_loc = cur_loc
+    @property
+    def cur_direc(self):
+        return self.__cur_direc
+    @cur_direc.setter
+    def cur_direc(self, cur_direc):
+        self.__cur_direc = cur_direc
     def appendRow(self):
         np.insert(self.grid, 0, np.full(len(self.grid[0]),State.UNKWN),axis=0)
     def appendCol(self):
@@ -85,10 +92,10 @@ class Map:
             np.append(r,State.UNKWN)
     def addPoint(self,pt,point_type = State.EXPL):
         if pt[0] > len(self.grid[0]):
-            appendCol()
-        elif pt[1] > len(self.grid)
-            appendRow
-        self.grid[-origin[1]][origin[0] - 1] = point_type
+            self.appendCol()
+        elif pt[1] > len(self.grid):
+            self.appendRow()
+        self.grid[-pt[1] - 1][pt[0]] = point_type
     def updateLocation(self):
         pass
     def findNearestUnexp(self,):
