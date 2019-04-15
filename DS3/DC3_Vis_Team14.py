5448c2dd36f1d72276dd56a5bbcc29415561d275
ihagedo <47994420+ihagedo@users.noreply.github.com>
2019-04-14 21:18:29 -0400

    vis

--- DS3/DC3_Vis_Team14.py
+++ DS3/DC3_Vis_Team14.py
@@ -4,31 +4,13 @@ import matplotlib.pyplot as plt
 
 file = open("DC3_Data.csv", "r")
 data = csv.reader(file)
-image = []
+
 img = []
-img1 = []
-img2 = []
-img3 = []
-img4 = []
 
 for row in data:
     img.append(float(row[0]))
-    img1.append(float(row[1]))
-    img2.append(float(row[2]))
-    img3.append(float(row[3]))
-    img4.append(float(row[4]))
-for x in range(0,5):
-    image.append(img[x])
-for x in range(0,5):
-    image.append(img1[x])
-for x in range(0,5):
-    image.append(img2[x])
-for x in range(0,5):
-    image.append(img3[x])
-for x in range(0,5):
-    image.append(img4[x])
-image = np.arange(25)
-i = image.reshape(5,5)
+    
+i = np.reshape(img, (25,25))
 data1 = np.array(i, dtype=float)
 plt.figure(1)
 plt.gray()
