c2d08a4f4f280218335a7e56859987fd9798b6d4
ihagedo <47994420+ihagedo@users.noreply.github.com>
2019-04-17 10:24:26 -0400

    haz

--- sensors.py
+++ sensors.py
@@ -10,6 +10,7 @@ from IMUFilters import KalmanFilter
 from IMUFilters import FindSTD
 from IMUFilters import InvGaussFilter
 from MPU9250 import MPU9250
+from mapping import State
 
 
 def gyroCalib(BP):
@@ -298,16 +299,24 @@ def hazardCheck(imu_calib, ir_thresh = 130 ,magx_thresh = 30 , magy_thresh = 115
             if ir_val[0] >= ir_thresh or ir_val[1] >= ir_thresh:
                 time.sleep(.5)
                 haz_ir_val = irVal()
-                haz = State.HEAT
-                magnitude = sqrt(ir_val[0]**2 + ir_val[1]**2)
+                if haz_ir_val[0] >= ir_thresh or haz_ir_val[1] >= ir_thresh:
+                    haz = State.HEAT
+                    magnitude = math.sqrt(ir_val[0]**2 + ir_val[1]**2)
+                else:
+                    haz = None
+                    magnitude = None
             elif mag_val[1] >= magy_thresh:
                 time.sleep(.5)
                 haz_mag_val = imuMagFiltered(imu_calib)
-                haz = State.Mag
-                magnitude = sqrt(mag_val[0]**2 + mag_val[1]**2 + mag_val[2]**2)
+                if haz_mag_val[1] >= magy_thresh:
+                    haz = State.Mag
+                    magnitude = math.sqrt(mag_val[0]**2 + mag_val[1]**2 + mag_val[2]**2)
+                else:
+                    haz = None
+                    magnitude = None
             else:
                 haz = None
-                magnitude = 0
+                magnitude = None
                 
         return [haz, magnitude]
     except Exception as error:
